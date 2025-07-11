59c8d73aa795a32600af88e66e6242b4ac555dde
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-11 18:18:48 +0900

    테스트 코드 삭제

--- src/test/java/eureca/capstone/project/batch/BatchApplicationTests.java
+++ src/test/java/eureca/capstone/project/batch/BatchApplicationTests.java
@@ -1,13 +1,8 @@
 package eureca.capstone.project.batch;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BatchApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
