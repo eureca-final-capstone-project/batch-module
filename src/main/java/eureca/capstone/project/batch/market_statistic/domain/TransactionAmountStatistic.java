6e0bb8131fac793cc18b24451745e45c5fd96478
so-yeon1 <psy92411@gmail.com>
2025-07-24 10:29:46 +0900

    feat: TransactionAmountStatistic 엔티티 수정

--- src/main/java/eureca/capstone/project/batch/market_statistic/domain/TransactionAmountStatistic.java
+++ src/main/java/eureca/capstone/project/batch/market_statistic/domain/TransactionAmountStatistic.java
@@ -1,6 +1,7 @@
 package eureca.capstone.project.batch.market_statistic.domain;
 
 import eureca.capstone.project.batch.common.entity.BaseEntity;
+import eureca.capstone.project.batch.transaction_feed.entity.SalesType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,7 +11,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Table(name = "transaction_amount_statistics", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"statics_time"})})
+        @UniqueConstraint(columnNames = {"statics_time", "sales_type_id"})})
 @Entity
 @Getter
 @Builder
@@ -24,6 +25,13 @@ public class TransactionAmountStatistic extends BaseEntity {
 
     @Column(name = "transaction_amount")
     private long transactionAmount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sales_type_id")
+    private SalesType salesType;
+
+    @Column(name = "statistic_type") // HOUR/DAY
+    private String statisticType;
 
     @Column(name = "statics_time")
     private LocalDateTime staticsTime;
